1b2f598562528c0d36c3ee84fc0ac3fe2193437b
Ink.whiteゞ <835868899@qq.com>
2020-09-01 14:38:29 +0800

    update ...

---
+++ 007Response/app.py
@@ -0,0 +1,13 @@
+from flask import Flask,Response
+# from werkzeug.wrappers import Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    resp = Response(response='HelloWorld!', status=200, content_type='text/html')
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
