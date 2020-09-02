30f9a8c05fd5bb421af1b6d27fc50011698e096e
Ink.whiteゞ <835868899@qq.com>
2020-09-02 15:15:54 +0800

    update ...

--- 006Flask.URL重定向/app.py
+++ 006Flask.URL重定向/app.py
@@ -1,5 +1,4 @@
 from flask import Flask,request,redirect,url_for
-
 app = Flask(__name__)
 
 @app.route('/')
--- 007Response/app.py
+++ 007Response/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,Response
+from flask import Flask,Response,make_response,jsonify,json
 # from werkzeug.wrappers import Response
 
 app = Flask(__name__)
@@ -8,5 +8,19 @@ def hello_world():
     resp = Response(response='HelloWorld!', status=200, content_type='text/html')
     return resp
 
+@app.route('/getJson/')
+def getJson():
+    dict = {
+        "username":"qianxun",
+        "password":"123456",
+    }
+    # 方法1
+    return jsonify(dict)
+
+    # 方法2
+    # return Response(json.dumps(dict),content_type='application/json')
+
+    # 方法3
+    # return  make_response(jsonify(dict),200)
 if __name__ == '__main__':
     app.run(debug=True)
