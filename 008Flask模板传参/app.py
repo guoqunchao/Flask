47b6179aaaa43bde82bd7e38eb2e84070d4d514b
Ink.whiteゞ <835868899@qq.com>
2020-09-02 17:43:01 +0800

    update ...

---
+++ 008Flask模板传参/app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    context = {
+        'username':'zhiliao',
+        'password':'123456',
+        'age':18,
+        'children':{
+            'name':'xiao a',
+            'heigh': 180
+        }
+    }
+    return render_template('index.html',**context)
+
+if __name__ == '__main__':
+    app.run(debug=True)
